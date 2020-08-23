fcee89124ac42aeb6174423c1ce0ab526a7aae55
dhm2013724 <656314839@qq.com>
2020-08-23 09:39:26 -0700

    fixed some bugs about mean and std in preprocess, ADD resnet50

--- eas/MNN/resnet50/resnet50_mnn.cpp
+++ eas/MNN/resnet50/resnet50_mnn.cpp
@@ -47,12 +47,11 @@ int main(int argc, const char* argv[]) {
     // config.backendConfig = &bnconfig;
     auto session = net->createSession(config);
 
-    auto input = net->getSessionInput(session, NULL);
+    auto input = net->getSessionInput(session, "input_tensor");
     auto shape = input->shape();
     shape[0]   = 1;
     net->resizeTensor(input, shape);
     net->resizeSession(session);
-    auto output = net->getSessionOutput(session, NULL);
 
 	static unsigned char labels[1000][256];
 	char line[256];
@@ -69,8 +68,7 @@ int main(int argc, const char* argv[]) {
 	fclose(fp3);
 
     {
-        auto dims    = input->shape();
-        int inputDim = 0;
+ //       auto dims    = input->shape();
         int size_w   = 0;
         int size_h   = 0;
         int bpp      = 0;
@@ -92,32 +90,16 @@ int main(int argc, const char* argv[]) {
             MNN_ERROR("Can't open %s\n", inputPatch);
             return 0;
         }
-        MNN_PRINT("origin size: %d, %d\n", width, height);
+        MNN_PRINT("origin size: %d, %d, %d\n", width, height, channel);
         Matrix trans;
-        // Set transform, from dst scale to src, the ways below are both ok
-#ifdef USE_MAP_POINT
-        float srcPoints[] = {
-            0.0f, 0.0f,
-            0.0f, (float)(height-1),
-            (float)(width-1), 0.0f,
-            (float)(width-1), (float)(height-1),
-        };
-        float dstPoints[] = {
-            0.0f, 0.0f,
-            0.0f, (float)(size_h-1),
-            (float)(size_w-1), 0.0f,
-            (float)(size_w-1), (float)(size_h-1),
-        };
-        trans.setPolyToPoly((Point*)dstPoints, (Point*)srcPoints, 4);
-#else
         trans.setScale((float)(width-1) / (size_w-1), (float)(height-1) / (size_h-1));
-#endif
+
         ImageProcess::Config config;
         config.filterType = BILINEAR;
-      //  float mean[3]     = {103.94f, 116.78f, 123.68f};
-        //float normals[3] = {0.017f, 0.017f, 0.017f};
-         float mean[3]     = {127.5f, 127.5f, 127.5f};
-         float normals[3] = {0.00785f, 0.00785f, 0.00785f};
+   //     float mean[3]     = {103.94f, 116.78f, 123.68f};
+	float mean[3]     = {123.68f, 116.78f, 103.94f};
+        float normals[3] = {1.0f, 1.0f, 1.0f};
+
         ::memcpy(config.mean, mean, sizeof(mean));
         ::memcpy(config.normal, normals, sizeof(normals));
         config.sourceFormat = RGBA;
@@ -125,16 +107,26 @@ int main(int argc, const char* argv[]) {
 
         std::shared_ptr<ImageProcess> pretreat(ImageProcess::create(config));
         pretreat->setMatrix(trans);
+
+//add s
+//	auto nhwcTensor = new Tensor(input, Tensor::CAFFE_C4);
+//	pretreat->convert((uint8_t*)inputImage, width, height, 0, nhwcTensor);
+//	input->copyFromHostTensor(nhwcTensor);
+//add e
         pretreat->convert((uint8_t*)inputImage, width, height, 0, input);
+
         stbi_image_free(inputImage);
     }
+
 	double time1, time2;
 	time1 = what_time_is_it_now();
-    net->runSession(session);
+    	net->runSession(session);
 	time2 = what_time_is_it_now();
 	printf("Mobilenet v1: %5.8lf s\n\n", time2 - time1);
 
-    {
+auto output = net->getSessionOutput(session, "softmax_tensor");
+
+ {
         auto dimType = output->getDimensionType();
         if (output->getType().code != halide_type_float) {
             dimType = Tensor::TENSORFLOW;
@@ -167,6 +159,7 @@ int main(int argc, const char* argv[]) {
 		MNN_PRINT("[%d]%s: %f\n", tempValues[i].first, labels[tempValues[i].first-1], tempValues[i].second);
 	    }
 
-    }
+}
+
     return 0;
 }
